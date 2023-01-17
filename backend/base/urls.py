7093007c72b7be5c2f4926542f66a732b05e0c6d
aryavaziri <arya.vaziri@gmail.com>
2023-01-17 15:11:15 +0100

    JWT customization

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('', views.getRoutes, name="routes"),
     path('tables/', views.getTables, name="tables"),
     path('tables/<str:pk>/', views.getTable, name="table"),
