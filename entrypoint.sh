3c3b570697368754fcd54d02031b21f115e21b84
ar <arya.vaziri@gmail.com>
2023-03-04 18:23:09 +0000

    Dockerr

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,6 @@
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
-
-DJANGO_SUPERUSER_PASSWORD=$SUPER_USER_PASSWORD 
 python manage.py createsuperuser --email $SUPER_USER_EMAIL --noinput
 
 daphne -b 0.0.0.0 -p 8000 backend.asgi:application
