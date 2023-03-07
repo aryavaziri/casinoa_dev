58abad43139329c5706d679d02d4cfea7c73601d
aryavaziri <arya.vaziri@gmail.com>
2023-03-07 17:26:54 +0100

    server2

--- frontend/src/constants/userConstants.js
+++ frontend/src/constants/userConstants.js
@@ -3,8 +3,8 @@ export const USER_LOGIN_SUCCESS = 'USER_LOGIN_SUCCESS'
 export const USER_LOGIN_FAIL = 'USER_LOGIN_FAIL'
 
 export const USER_LOGOUT = 'USER_LOGOUT'
-// export const hostname = window.location.host
-export const hostname = window.location.hostname + ":8000"
+export const hostname = window.location.host
+// export const hostname = window.location.hostname + ":8000"
 
 export const USER_REGISTER_REQUEST = 'USER_REGISTER_REQUEST'
 export const USER_REGISTER_SUCCESS = 'USER_REGISTER_SUCCESS'
